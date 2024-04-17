52dd18e42c5287a652538d8999a412c6eef7ff82
Joakim Repomaa <joakim@repomaa.com>
2024-04-17 16:00:20 +0300

    add sentry

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,5 @@
+import {sequence} from '@sveltejs/kit/hooks';
+import * as Sentry from '@sentry/sveltekit';
 import { env } from '$env/dynamic/private'
 import { Pool, neonConfig } from '@neondatabase/serverless'
 import type { Handle } from '@sveltejs/kit'
@@ -5,7 +7,12 @@ import { drizzle } from 'drizzle-orm/neon-serverless'
 import { schema } from '$lib/server/db'
 import { dev } from '$app/environment'
 
-export const handle: Handle = async ({ event, resolve }) => {
+Sentry.init({
+    dsn: "https://c576d0d3a3ea50f83f647bb4dff761f5@o4507101945462784.ingest.de.sentry.io/4507101947560016",
+    tracesSampleRate: 1
+})
+
+export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
   if (dev) {
     const { default: ws } = await import('ws')
     neonConfig.webSocketConstructor = ws
@@ -17,4 +24,5 @@ export const handle: Handle = async ({ event, resolve }) => {
   const response = await resolve(event)
   await pool.end()
   return response
-}
+})
+export const handleError = Sentry.handleErrorWithSentry();
